abf88e310c7df755949d51e5b57036005dba3998
Harihara-suthan2004 <harigomathi20@gmail.com>
2025-06-26 18:42:33 +0530

    worked on scipt

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const bar=document.getElementById('menuicon');
+const closed=document.getElementById('close');
+const nav=document.getElementById('h-List');
+if(bar){
+    bar.addEventListener('click',()=>{
+    nav.classList.add('flag');
+    });
+}
+if(close){
+    closed.addEventListener('click',()=>{
+    nav.classList.remove('flag');
+    });
+}
+
 
